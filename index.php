ebe1192306c8193e6d17689e8a171e2be3154407
roik <victorio80@list.ru>
2017-04-29 22:08:57 +0300

    test

--- index.php
+++ index.php
@@ -7,34 +7,49 @@
  */
 echo "HELLO WORLD";
 
-//$dt = new DateTime('Wed, 21 Jul 2017 00:28:50 GMT');
-//echo $dt->format('U'); // 1279672130
-//echo $dt->getTimestamp();
 ?>
 <form method="post" action="index.php">
     <input type="text" placeholder="name" name "name">
     <br>
     <input type="text" placeholder="description" name "description">
     <br>
-<!--    <input type="text" placeholder="created_at" name "created_at">-->
+   <input type="text" placeholder="created_at" name "created_at">
     <br>
     <input type="submit">
 </form>
 
 <?php
-$pdo_conn = new PDO("pgsql:host=127.0.0.1;dbname=grud", 'postgres', 'qwerty');
 
- /* var_dump($pdo_conn::getAvailableDrivers());*/
-if (isset($_POST['name']) && isset($_POST['description']))
-{
-    $sql = 'INSERT INTO article (name, description) VALUES (:name, :description)';
-    $statement = $pdo_conn->prepare($sql);
-    $statement->bindValue(':name', $_POST['name']);
-    $statement->bindValue(':description', $_POST['description']);
-    /*$statement->bindValue(':created_at', $_POST['created_at']);
-    */
-    var_dump($statement->execute());
+$dsn = 'mysql:dbname=CRUD;host=127.0.0.1';
+$user = 'root';
+$password = 'root';
+
+try {
+     $dbh = new PDO($dsn, $user, $password);
+    }
+    catch (PDOException $e) {
+        echo 'Подключение не удалось: ' . $e->getMessage();
 }
+        $sql = 'INSERT INTO article (name,description,created_at) VALUES (:name,:description,:created_at)';
+        $statement =$dbh->prepare($sql);
 
+        if (!empty($statement))
+
+            {
+                echo 'Вставили запись ';
+               /* $statement->bindParam(':name', $_POST['name'], PDO::PARAM_STR);
+                $statement->bindParam(':description', $_POST['description'], PDO::PARAM_STR);
+                $statement->bindParam(':created_at', $_POST['created_at'], PDO::PARAM_STR);*/
+                $statement->bindValue(':name', $_POST['name']);
+                $statement->bindValue(':description', $_POST['description']);
+                $statement->bindValue(':created_at', $_POST['created_at']);
+
+                var_dump($statement->execute());
+            }
+            else
+            {
+                echo 'Banana!';
+
+            }
 ?>
 
